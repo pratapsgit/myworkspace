e7f01207493bd4a472653fb0960829efb2c1bb2c
pratapsgit <pk1979m@gmail.com>
2025-03-25 07:34:06 +0530

    started data science math

---
+++ daily_activity/excercises/ds_python/math_01/math_01.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+mat_a = np.array([[1, 2], [3, 4]])
+mat_b = np.array([[5, 6], [7, 8]])
+
+print("matrix a ")
+print(mat_a)
+
+
+print("matrix b ")
+print(mat_b)
+
+print("matrix addition")
+c = mat_a + mat_b
+print(c)
